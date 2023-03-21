d1232ef9a1603f0323d4b1d5e5cf914273f304cd
Audrey Kurniawan <audrey.kurniawan03@gmail.com>
2023-03-21 17:28:38 +0800

    minor changes

--- databaseConn/test.py
+++ databaseConn/test.py
@@ -49,7 +49,7 @@ def get_all():
     return jsonify(
         {
             "code": 404,
-            "message": "There are no students."
+            "message": "There are no orders found."
         }
     ), 404
 
