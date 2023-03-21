37ec781235f7e4bf8dd30aaceb56689edcf44fb5
Audrey Kurniawan <audrey.kurniawan03@gmail.com>
2023-03-21 16:31:26 +0800

    linked test database for orders

--- databaseConn/test.py
+++ databaseConn/test.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://admin:rootroot@db-savood.c0hav88yk9mq.us-east-1.rds.amazonaws.com:3306/test"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://admin:rootroot@db-savood.c0hav88yk9mq.us-east-1.rds.amazonaws.com:3306/order"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
@@ -11,29 +11,38 @@ db = SQLAlchemy(app)
 
 CORS(app)
 
-class Student(db.Model):
-    __tablename__ = 'student'
+class Order(db.Model):
+    __tablename__ = 'orders'
 
-    sid = db.Column(db.String(5), primary_key=True)
-    sname = db.Column(db.String(64), nullable=False)
+    order_id = db.Column(db.Integer, primary_key=True)
+    foodbank_id = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, nullable=False)
+    dish_id = db.Column(db.Integer, nullable=False)
+    quantity_check = db.Column(db.SmallInteger, nullable=False)
+    status = db.Column(db.Enum, nullable=False)
 
-    def __init__(self, sid, sname):
-        self.sid = sid
-        self.sname = sname
+    def __init__(self, order_id, foodbank_id, restaurant_id, dish_id, quantity_check, status):
+        self.order_id = order_id
+        self.foodbank_id = foodbank_id
+        self.restaurant_id = restaurant_id
+        self.dish_id = dish_id
+        self.quantity_check = quantity_check
+        self.status = status
+        
 
     def json(self):
-        return {"sid": self.sid, "sname": self.sname}
+        return {"order_id": self.order_id, "foodbank_id": self.foodbank_id, "restaurant_id": self.restaurant_id, "dish_id": self.dish_id, "quantity_check": self.quantity_check, "status": self.status}
 
 
-@app.route("/students")
+@app.route("/orders")
 def get_all():
-    studentlist = Student.query.all()
-    if len(studentlist):
+    orderList = Order.query.all()
+    if len(orderList):
         return jsonify(
             {
                 "code": 200,
                 "data": {
-                    "students": [student.json() for student in studentlist]
+                    "orders": [order.json() for order in orderList]
                 }
             }
         )
