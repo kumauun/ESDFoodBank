0d5621c193e243f0ea64a9879f26e410cdb578bb
SSSamuelDS <samuel240303@gmail.com>
2023-04-01 01:22:23 +0800

    place order

--- place-order/placeOrder.py
+++ place-order/placeOrder.py
@@ -5,27 +5,85 @@ import os, sys
 from os import environ
 
 import requests
-
+from invokes import invoke_http
 ##import amqp_setup
 import pika
 import json
 
 app = Flask(__name__)
 CORS(app)
+restaurant_URL="http://localhost:5001/restaurant"
+foodbank_URL= "http://localhost:5002/foodbank"
+orderManagement_URL="http://localhost:5005/orderManagement"
 
 #dapet dari foodbank UI, kirim request ke order management for update order, receive reponse 
 #dari order management, fire back to foodbank UI
-@app.route("/place_order", methods=['PUT'])
-def accept_order():
-    pass
-    # 1. retrieve phone number of foodbank that places the order from the foodbank table
-    # 2. update order status to 'ordered' and order foodbank number with the #1 output
-    # 3. retrieve resto phone number from order table  
-    # 4. notify restoran yang bersangkutan
-    # 5. retrieve phone number driver yang ada di regionnya
-    # 6. notify driver yang ada di regionnya
-    # 6. Send back response to foodbank UI 
+# Function to update the order in the order management microservice
+def update_order(order_id, foodbank_phone):
+    url = orderManagement_URL + "/update_order"
+    headers = {'Content-Type': 'application/json'}
+    data = {'order_id': order_id, 'status': 'ordered', 'foodbank_phone': foodbank_phone}
+    response = requests.put(url, headers=headers, json=data)
+    return response.json()
 
+
+# Function to notify the restaurant about the new order
+def notify_restaurant(restaurant_phone, order_id):
+    url = restaurant_URL + "/notify"
+    headers = {'Content-Type': 'application/json'}
+    data = {'order_id': order_id}
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
+
+
+# Function to retrieve driver phone numbers in the same region as the foodbank
+def get_drivers(region):
+    url = orderManagement_URL + "/get_drivers"
+    headers = {'Content-Type': 'application/json'}
+    data = {'region': region}
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
+
+
+# Function to notify drivers about the new order
+def notify_drivers(drivers, order_id):
+    url = orderManagement_URL + "/notify_drivers"
+    headers = {'Content-Type': 'application/json'}
+    data = {'order_id': order_id, 'drivers': drivers}
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
+
+
+# Place order API endpoint
+@app.route("/place_order", methods=['PUT'])
+def place_order():
+    # 1. retrieve phone number of foodbank that places the order from the foodbank table
+    foodbank_id = request.json['foodbank_id']
+    response = requests.get(foodbank_URL + "/get_phone_number/" + foodbank_id)
+    foodbank_phone = response.json()['phone_number']
+
+    # 2. update order status to 'ordered' and order foodbank number with the #1 output
+    order_id = request.json['order_id']
+    update_order(order_id, foodbank_phone)
+
+    # 3. retrieve restaurant phone number from order table
+    response = requests.get(orderManagement_URL + "/get_order/" + order_id)
+    restaurant_id = response.json()['restaurant_id']
+    response = requests.get(restaurant_URL + "/get_phone_number/" + restaurant_id)
+    restaurant_phone = response.json()['phone_number']
+
+    # 4. notify involved restaurant
+    notify_restaurant(restaurant_phone, order_id)
+
+    # 5. retrieve phone number driver yang ada di regionnya
+    foodbank_region = request.json['foodbank_region']
+    drivers = get_drivers(foodbank_region)
+
+    # 6. notify driver yang ada di regionnya
+    notify_drivers(drivers, order_id)
+
+    # 7. Send back response to foodbank UI
+    return jsonify({'message': 'Order placed successfully.'})
 @app.route("/load_listings", methods=['GET'])
 def load_listings():
     pass
